a78bf8e068982901b75c07cf4df2e85bf73dc4fe
idrissa mulenga <1mulengaidriss@gmail.com>
2026-06-28 16:31:22 +0200

    added new line

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,38 @@
-import express from "express";
+import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from "mongoose";
-import cors from "cors";
-import { createSchema, createYoga } from "graphql-yoga";
-import { typeDefs } from "./graphql/schemas/index.js";
-import { resolvers } from "./graphql/resolvers/index.js";
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { createSchema, createYoga } from 'graphql-yoga';
+import { typeDefs } from './graphql/schemas/index.js';
+import { resolvers } from './graphql/resolvers/index.js';
 
 // connect to link variables
 dotenv.config();
 
 // connect to express server
 const app = express();
+const expressapp = express();
 
 // middleware
 app.use(cors());
 
 const schema = createSchema({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
 const Yoga = createYoga({
-    schema
-})
+  schema,
+});
 
+app.use('/graphql', Yoga);
 
-app.use("/graphql", Yoga);
+const MONGODB = process.env.MONGODB_URI;
 
-
-const MONGODB = process.env.MONGODB_URI
-
-mongoose.connect(MONGODB as string)
-    .then(() => {
-        const port = process.env.PORT
-        app.listen(port, () => console.log(`Server running on port ${port}.....`));
-    })
-    .catch((err) => console.log(err.message))
+mongoose
+  .connect(MONGODB as string)
+  .then(() => {
+    const port = process.env.PORT;
+    app.listen(port, () => console.log(`Server running on port ${port}.....`));
+  })
+  .catch((err) => console.log(err.message));
